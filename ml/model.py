a17b1bbe893d99d457f988025311b4486234c651
KremlevLev <kremlevlev888@gmail.com>
2026-08-01 12:34:32 +0300

    .

--- ml/model.py
+++ ml/model.py
@@ -149,14 +149,14 @@ test_transforms = transforms.Compose([
 ])
 
 train_dataset = datasets.CIFAR10(
-    root='./data',   
+    root='/kaggle/working/',   
     train=True,           
     download=True,         
     transform=train_transforms
 )
 
 test_dataset = datasets.CIFAR10(
-    root='./data',   
+    root='/kaggle/working/',   
     train=False,           
     download=True,         
     transform=test_transforms

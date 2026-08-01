8fe3031a7653eb23e43c3eb87100a75e0132af2a
KremlevLev <kremlevlev888@gmail.com>
2026-08-01 12:22:24 +0300

    add torch cnn train

--- ml/model.py
+++ ml/model.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
 class ResidualDepthwiseBlock(nn.Module):
     def __init__(self, channels: int):
@@ -129,3 +131,114 @@ x = torch.randn(8, 3, 32, 32)
 logits = model(x)
 
 print(logits.shape)
+
+#DATA
+mean = (0.4914, 0.4822, 0.4465)
+std = (0.2470, 0.2435, 0.2616)
+
+train_transforms = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std),
+])
+
+test_transforms = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std),
+])
+
+train_dataset = datasets.CIFAR10(
+    root='./data',   
+    train=True,           
+    download=True,         
+    transform=train_transforms
+)
+
+test_dataset = datasets.CIFAR10(
+    root='./data',   
+    train=False,           
+    download=True,         
+    transform=test_transforms
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=128,
+    shuffle=True,
+    num_workers=0,
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=128,
+    shuffle=False,
+    num_workers=0,
+)
+
+device = torch.device(
+    "cuda" if torch.cuda.is_available() else "cpu"
+)
+
+model = model.to(device)
+
+#model
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=3e-4,
+    weight_decay=1e-4,
+)
+
+#train
+
+model.train()
+num_epochs = 10
+running_loss = 0.0
+correct,total=0,0
+
+for epoch in range(num_epochs):
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+    
+        optimizer.zero_grad()
+    
+        logits = model(images)
+        loss = criterion(logits, labels)
+    
+        loss.backward()
+        optimizer.step()
+        if total%100==0:
+            print(loss.item())
+        running_loss += loss.item()
+        predictions = logits.argmax(dim=1)
+        correct += (predictions == labels).sum().item()
+        total += labels.size(0)
+
+accuracy = correct / total
+train_loss = running_loss / len(train_loader)
+
+model.eval()
+
+test_loss_sum = 0.0
+test_correct = 0
+test_total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        logits = model(images)
+        loss = criterion(logits, labels)
+
+        test_loss_sum += loss.item()
+        predictions = logits.argmax(dim=1)
+        test_correct += (predictions == labels).sum().item()
+        test_total += labels.size(0)
+
+test_accuracy = test_correct / test_total
+test_loss = test_loss_sum / len(test_loader)
+
